a27b2bac0ee321d21dfa2dd1b1969be1dd9231ee
XuanLam11 <Lamdoxuan9@gmail.com>
2024-12-25 15:35:25 +0700

    push

---
+++ app/Models/Book.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Book extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'author',
+        'category',
+        'year',
+        'quantity',
+    ];
+    public function borrows()
+    {
+        return $this ->hasMany(Borrow::class);
+
+    }
+}
